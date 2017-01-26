e62827ad3f8a967027fc626de762e5fed2a4664a
richhall <rhllrichard@gmail.com>
2017-01-26 17:00:39 +0000

    added multiple markers in same location

---
+++ scripts/associates_stacked_geojsonp.js
@@ -0,0 +1,124 @@
+// 20170126125552
+// https://raw.githubusercontent.com/collusion-org-uk/collusion-network-map/master/scripts/associates.geojsonp
+
+eqfeed_callback({
+  "type": "FeatureCollection",
+  "metadata": {
+    "generated": "2017-01-26 13:50",
+    "url": "https://raw.githubusercontent.com/collusion-org-uk/collusion-network-map/master/scripts/associates.geojsonp",
+    "title": "Associates",
+    "status": 200,
+    "api": "0.1",
+    "count": 3
+  },
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "John",
+        "about": "Artist/maker",
+        "base": "St Ives, Cambs",
+		"colluding in": "Huntingdon",
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.07858809999999999,
+		  52.3347051
+        ]
+      },
+      "id": "1"
+    },
+	{
+      "type": "Feature",
+      "properties": {
+        "name": "Rich",
+        "about": "tech guy",
+        "base": "Cambridge",
+		"colluding in": "Wisbech, King's Lynn",
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          0.121817,
+		  52.205337
+        ]
+      },
+      "id": "2"
+    },
+	{
+      "type": "Feature",
+      "properties": {
+        "name": "Jane",
+        "about": "Artist",
+        "base": "Ely",
+		"colluding in": "Cambridge",
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          0.262363,
+		  52.399539,
+        ]
+      },
+      "id": "3"
+    },
+		{
+      "type": "Feature",
+      "properties": {
+        "name": "Rich",
+        "about": "tech guy",
+        "base": "Cambridge",
+		"colluding in": "Wisbech, King's Lynn",
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          0.121817,
+		  52.205337
+        ]
+      },
+      "id": "4"
+    },
+		{
+      "type": "Feature",
+      "properties": {
+        "name": "James",
+        "about": "maker",
+        "base": "Cambridge",
+		"colluding in": "Peterborough",
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          0.121817,
+		  52.205337
+        ]
+      },
+      "id": "5"
+    },
+		{
+      "type": "Feature",
+      "properties": {
+        "name": "Clare",
+        "about": "artist",
+        "base": "Cambridge",
+		"colluding in": "Huntingdon",
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          0.121817,
+		  52.205337
+        ]
+      },
+      "id": "6"
+    }
+  ],
+  "bbox": [
+    -0.07858809999999999,
+    52.205337,
+    0.262363,
+    52.399539
+  ]
+});
