6671e86be2c9b08d358dd6351c126b1a074b5458
richhall <rhllrichard@gmail.com>
2017-01-27 15:47:55 +0000

    added hubs

---
+++ scripts/hubs_geojsonp.js
@@ -0,0 +1,106 @@
+// 20170126125552
+// https://raw.githubusercontent.com/collusion-org-uk/collusion-network-map/master/scripts/hubs_geojsonp.js
+
+eqfeed_callback({
+  "type": "FeatureCollection",
+  "metadata": {
+    "generated": "2017-01-26 13:50",
+    "url": "https://raw.githubusercontent.com/collusion-org-uk/collusion-network-map/master/scripts/hubs_geojsonp.js",
+    "title": "Hubs",
+    "status": 200,
+    "api": "0.1",
+    "count": 6
+  },
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Cambridge",
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+           0.121817,
+           52.205337
+        ]
+      },
+      "id": "1"
+    },
+	{
+      "type": "Feature",
+      "properties": {
+        "name": "Huntingdon",
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+           -0.182552,
+			52.33146
+        ]
+      },
+      "id": "2"
+    },
+	{
+      "type": "Feature",
+      "properties": {
+        "name": "Peterborough",
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+           -0.2405299
+			52.56949849999999
+        ]
+      },
+      "id": "3"
+    },
+	{
+      "type": "Feature",
+      "properties": {
+        "name": "Wisbech",
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+			0.158797,
+			52.666317
+        ]
+      },
+      "id": "4"
+    },
+	{
+      "type": "Feature",
+      "properties": {
+        "name": "King's Lynn",
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+			0.4022963000000001,
+			52.7516798
+        ]
+      },
+      "id": "5"
+    },
+	{
+      "type": "Feature",
+      "properties": {
+        "name": "Bury St Edmunds",
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+			0.7104931999999999,
+			52.2429228
+        ]
+      },
+      "id": "6"
+    }
+  ],
+  "bbox": [
+    -0.2405299,
+    52.205337,
+    0.7104931999999999,
+    52.7516798
+  ]
+});
