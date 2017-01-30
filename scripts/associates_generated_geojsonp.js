4900df69589a89240cdb714ef3b31e37b88c8db8
richhall <rhllrichard@gmail.com>
2017-01-30 17:49:18 +0000

    test generated feed

---
+++ scripts/associates_generated_geojsonp.js
@@ -0,0 +1,67 @@
+eqfeed_callback({
+"type": "FeatureCollection",
+"metadata": {
+"generated": "2017-01-26 13:50",
+"url": "https://raw.githubusercontent.com/collusion-org-uk/collusion-network-map/master/scripts/associates.geojsonp",
+"title": "Associates",
+"status": 200,
+"api": "0.1",
+"count": 3
+},
+"features": [
+{
+"type": "Feature",
+"properties": {
+"name": "John",
+"about": "Artist/maker",
+"base": "St Ives, Cambs",
+"colluding in": "Huntingdon",
+},
+"geometry": {
+"type": "Point",
+"coordinates": [
+-0.0763979,
+52.3325252
+]
+}
+} 
+,,{
+"type": "Feature",
+"properties": {
+"name": "Rich",
+"about": "tech guy",
+"base": "Cambridge",
+"colluding in": "Wisbech, King's Lynn",
+},
+"geometry": {
+"type": "Point",
+"coordinates": [
+-71.10973349999999,
+42.3736158
+]
+}
+} 
+,,{
+"type": "Feature",
+"properties": {
+"name": "Jane",
+"about": "Artist",
+"base": "Ely",
+"colluding in": "Cambridge",
+},
+"geometry": {
+"type": "Point",
+"coordinates": [
+-91.8670873,
+47.9032372
+]
+}
+} 
+],
+"bbox": [
+undefined,
+-0.0763979,
+undefined,
+47.9032372
+]
+});
